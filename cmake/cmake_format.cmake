b638f2de0fbb056763b36597a26126df8739a04d
Kyrylo Rud <krud.official@gmail.com>
2021-07-09 01:20:04 +0300

    cmake: setup the cmake format target

--- cmake/cmake_format.cmake
+++ cmake/cmake_format.cmake
@@ -14,19 +14,20 @@
 # limitations under the License.
 ################################################################################
 
-find_program(clang-format NAMES clang-format REQUIRED)
+find_program(cmake-format NAMES cmake-format REQUIRED)
 
-set(CXX_FILE_EXTENSIONS "C|c|c++|cc|cpp|cxx|h|H|h++|hh|hpp|hxx")
-set(CXX_FILE_REGEX ".*\\.(${CXX_FILE_EXTENSIONS})$")
-set(CXX_FIND_INC "-regex '${CXX_FILE_REGEX}'")
-set(CXX_FIND_EXC "-not -path '${CMAKE_BINARY_DIR}/*'")
-set(CXX_FIND_CLI "find ${CMAKE_SOURCE_DIR} ${CXX_FIND_INC} ${CXX_FIND_EXC}")
-string(REPLACE "|" "\\|" CXX_FIND_CLI ${CXX_FIND_CLI})
-string(REPLACE "(" "\\(" CXX_FIND_CLI ${CXX_FIND_CLI})
-string(REPLACE ")" "\\)" CXX_FIND_CLI ${CXX_FIND_CLI})
+set(CMAKE_FILE_REGEX ".*(CMakeLists\\.txt|\\.cmake)$")
+set(CMAKE_FIND_INC "-regex '${CMAKE_FILE_REGEX}'")
+set(CMAKE_FIND_EXC "-not -path '${CMAKE_BINARY_DIR}/*'")
+set(CMAKE_FIND_CLI
+    "find ${CMAKE_SOURCE_DIR} ${CMAKE_FIND_INC} ${CMAKE_FIND_EXC}"
+)
+string(REPLACE "|" "\\|" CMAKE_FIND_CLI ${CMAKE_FIND_CLI})
+string(REPLACE "(" "\\(" CMAKE_FIND_CLI ${CMAKE_FIND_CLI})
+string(REPLACE ")" "\\)" CMAKE_FIND_CLI ${CMAKE_FIND_CLI})
 
 add_custom_target(
-   clang_format
+   cmake_format
    VERBATIM
-   COMMAND bash -c "${CXX_FIND_CLI} | xargs -n 1 ${clang-format} -i"
+   COMMAND bash -c "${CMAKE_FIND_CLI} | xargs -n 1 ${cmake-format} -i"
 )
